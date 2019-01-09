484a03fbf21eadec5bca39eb55a5f4cadaa1cc97
zzzhr <zzzhr@hotmail.com>
2019-01-09 20:32:59 +0800

    change to identity

--- go/services/userfile/userfile.pb.go
+++ go/services/userfile/userfile.pb.go
@@ -23,25 +23,23 @@ var _ = math.Inf
 const _ = proto.ProtoPackageIsVersion3 // please upgrade the proto package
 
 type GrpcUserFile struct {
-	UUID    string `protobuf:"bytes,1,opt,name=UUID,proto3" json:"UUID,omitempty"`
-	StoreID string `protobuf:"bytes,2,opt,name=StoreID,proto3" json:"StoreID,omitempty"`
-	UserID  uint64 `protobuf:"varint,3,opt,name=UserID,proto3" json:"UserID,omitempty"`
-	Path    string `protobuf:"bytes,4,opt,name=Path,proto3" json:"Path,omitempty"`
-	Name    string `protobuf:"bytes,5,opt,name=Name,proto3" json:"Name,omitempty"`
-	Ext     string `protobuf:"bytes,6,opt,name=Ext,proto3" json:"Ext,omitempty"`
-	Size    uint64 `protobuf:"varint,7,opt,name=Size,proto3" json:"Size,omitempty"`
-	Parent  string `protobuf:"bytes,8,opt,name=Parent,proto3" json:"Parent,omitempty"`
-	//string createTime = 7;
-	Type int32 `protobuf:"varint,9,opt,name=Type,proto3" json:"Type,omitempty"`
-	//string ssid = 10;
-	IsDirectory          bool     `protobuf:"varint,10,opt,name=IsDirectory,proto3" json:"IsDirectory,omitempty"`
-	Atime                uint64   `protobuf:"varint,11,opt,name=Atime,proto3" json:"Atime,omitempty"`
-	Ctime                uint64   `protobuf:"varint,12,opt,name=Ctime,proto3" json:"Ctime,omitempty"`
-	Mtime                uint64   `protobuf:"varint,13,opt,name=Mtime,proto3" json:"Mtime,omitempty"`
-	Version              uint32   `protobuf:"varint,14,opt,name=Version,proto3" json:"Version,omitempty"`
-	Locking              bool     `protobuf:"varint,15,opt,name=Locking,proto3" json:"Locking,omitempty"`
-	Op                   uint32   `protobuf:"varint,16,opt,name=Op,proto3" json:"Op,omitempty"`
-	IgnoreCase           bool     `protobuf:"varint,17,opt,name=IgnoreCase,proto3" json:"IgnoreCase,omitempty"`
+	Identity             string   `protobuf:"bytes,1,opt,name=identity,proto3" json:"identity,omitempty"`
+	StoreIdentity        string   `protobuf:"bytes,2,opt,name=storeIdentity,proto3" json:"storeIdentity,omitempty"`
+	UserIdentity         uint64   `protobuf:"varint,3,opt,name=userIdentity,proto3" json:"userIdentity,omitempty"`
+	Path                 string   `protobuf:"bytes,4,opt,name=path,proto3" json:"path,omitempty"`
+	Name                 string   `protobuf:"bytes,5,opt,name=name,proto3" json:"name,omitempty"`
+	Ext                  string   `protobuf:"bytes,6,opt,name=ext,proto3" json:"ext,omitempty"`
+	Size                 uint64   `protobuf:"varint,7,opt,name=size,proto3" json:"size,omitempty"`
+	Parent               string   `protobuf:"bytes,8,opt,name=parent,proto3" json:"parent,omitempty"`
+	Type                 int32    `protobuf:"varint,9,opt,name=type,proto3" json:"type,omitempty"`
+	IsDirectory          bool     `protobuf:"varint,10,opt,name=isDirectory,proto3" json:"isDirectory,omitempty"`
+	Atime                uint64   `protobuf:"varint,11,opt,name=atime,proto3" json:"atime,omitempty"`
+	Ctime                uint64   `protobuf:"varint,12,opt,name=ctime,proto3" json:"ctime,omitempty"`
+	Mtime                uint64   `protobuf:"varint,13,opt,name=mtime,proto3" json:"mtime,omitempty"`
+	Version              uint32   `protobuf:"varint,14,opt,name=version,proto3" json:"version,omitempty"`
+	Locking              bool     `protobuf:"varint,15,opt,name=locking,proto3" json:"locking,omitempty"`
+	Op                   uint32   `protobuf:"varint,16,opt,name=op,proto3" json:"op,omitempty"`
+	IgnoreCase           bool     `protobuf:"varint,17,opt,name=ignoreCase,proto3" json:"ignoreCase,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
 	XXX_unrecognized     []byte   `json:"-"`
 	XXX_sizecache        int32    `json:"-"`
@@ -72,23 +70,23 @@ func (m *GrpcUserFile) XXX_DiscardUnknown() {
 
 var xxx_messageInfo_GrpcUserFile proto.InternalMessageInfo
 
-func (m *GrpcUserFile) GetUUID() string {
+func (m *GrpcUserFile) GetIdentity() string {
 	if m != nil {
-		return m.UUID
+		return m.Identity
 	}
 	return ""
 }
 
-func (m *GrpcUserFile) GetStoreID() string {
+func (m *GrpcUserFile) GetStoreIdentity() string {
 	if m != nil {
-		return m.StoreID
+		return m.StoreIdentity
 	}
 	return ""
 }
 
-func (m *GrpcUserFile) GetUserID() uint64 {
+func (m *GrpcUserFile) GetUserIdentity() uint64 {
 	if m != nil {
-		return m.UserID
+		return m.UserIdentity
 	}
 	return 0
 }
@@ -198,33 +196,33 @@ func init() {
 func init() { proto.RegisterFile("user/userfile.proto", fileDescriptor_d6806edd9a7324ee) }
 
 var fileDescriptor_d6806edd9a7324ee = []byte{
-	// 403 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xa4, 0x92, 0xcd, 0x8e, 0xd3, 0x30,
-	0x14, 0x85, 0x49, 0xff, 0xa6, 0x73, 0x3b, 0xd3, 0x19, 0x0c, 0x1a, 0x5d, 0xb1, 0x40, 0xd1, 0xac,
-	0xb2, 0x21, 0xe1, 0x47, 0x08, 0x75, 0x85, 0x68, 0xd3, 0xa2, 0x48, 0x40, 0xab, 0x94, 0xb2, 0x60,
-	0x97, 0x5a, 0x97, 0xd4, 0xa2, 0x89, 0x23, 0xc7, 0x45, 0x34, 0x0f, 0xc2, 0xe3, 0xf1, 0x2c, 0xc8,
-	0x4e, 0x53, 0x65, 0xc3, 0x02, 0xb1, 0x89, 0xce, 0xf9, 0x72, 0x7d, 0xae, 0xe4, 0x63, 0x78, 0x74,
-	0x28, 0x49, 0x05, 0xe6, 0xf3, 0x4d, 0xec, 0xc9, 0x2f, 0x94, 0xd4, 0x92, 0x0d, 0x4b, 0x52, 0x3f,
-	0x04, 0xa7, 0xf2, 0xfe, 0x57, 0x17, 0xae, 0xde, 0xab, 0x82, 0x6f, 0x4a, 0x52, 0x0b, 0xb1, 0x27,
-	0xc6, 0xa0, 0xb7, 0xd9, 0x44, 0x21, 0x3a, 0xae, 0xe3, 0x5d, 0xc6, 0x56, 0x33, 0x84, 0x8b, 0xb5,
-	0x96, 0x8a, 0xa2, 0x10, 0x3b, 0x16, 0x37, 0x96, 0xdd, 0xc1, 0xc0, 0x9c, 0x8c, 0x42, 0xec, 0xba,
-	0x8e, 0xd7, 0x8b, 0x4f, 0xce, 0xa4, 0xac, 0x12, 0xbd, 0xc3, 0x5e, 0x9d, 0x62, 0xb4, 0x61, 0x9f,
-	0x92, 0x8c, 0xb0, 0x5f, 0x33, 0xa3, 0xd9, 0x2d, 0x74, 0xe7, 0x3f, 0x35, 0x0e, 0x2c, 0x32, 0xd2,
-	0x4c, 0xad, 0x45, 0x45, 0x78, 0x61, 0xf3, 0xac, 0x36, 0x5b, 0x56, 0x89, 0xa2, 0x5c, 0xe3, 0xd0,
-	0x0e, 0x9e, 0x9c, 0x99, 0xfd, 0x7c, 0x2c, 0x08, 0x2f, 0x5d, 0xc7, 0xeb, 0xc7, 0x56, 0x33, 0x17,
-	0x46, 0x51, 0x19, 0x0a, 0x45, 0x5c, 0x4b, 0x75, 0x44, 0x70, 0x1d, 0x6f, 0x18, 0xb7, 0x11, 0x7b,
-	0x0c, 0xfd, 0x77, 0x5a, 0x64, 0x84, 0x23, 0xbb, 0xa2, 0x36, 0x86, 0xce, 0x2c, 0xbd, 0xaa, 0xe9,
-	0xac, 0xa1, 0x1f, 0x2d, 0xbd, 0xae, 0xa9, 0x35, 0xe6, 0x3e, 0xbe, 0x90, 0x2a, 0x85, 0xcc, 0x71,
-	0xec, 0x3a, 0xde, 0x75, 0xdc, 0x58, 0xf3, 0xe7, 0x83, 0xe4, 0xdf, 0x45, 0x9e, 0xe2, 0x8d, 0xdd,
-	0xdc, 0x58, 0x36, 0x86, 0xce, 0xb2, 0xc0, 0x5b, 0x3b, 0xde, 0x59, 0x16, 0xec, 0x29, 0x40, 0x94,
-	0xe6, 0x52, 0xd1, 0x2c, 0x29, 0x09, 0x1f, 0xda, 0xe1, 0x16, 0x79, 0xf9, 0xdb, 0x81, 0x9b, 0xa6,
-	0x98, 0x75, 0xdd, 0x16, 0x9b, 0xc2, 0x98, 0x2b, 0x4a, 0x34, 0x9d, 0xdb, 0xba, 0xf3, 0x9b, 0x26,
-	0xfd, 0x76, 0x8b, 0x4f, 0xfe, 0xc2, 0xef, 0x1f, 0xb0, 0xb7, 0x30, 0x4a, 0x49, 0xff, 0x47, 0xc0,
-	0x02, 0x58, 0x2b, 0x60, 0xa9, 0xe6, 0x59, 0xa1, 0x8f, 0xff, 0x9e, 0x33, 0x7d, 0xf3, 0xf5, 0x75,
-	0x2a, 0xf4, 0xee, 0xb0, 0xf5, 0xb9, 0xcc, 0x82, 0xaa, 0xaa, 0x76, 0xea, 0xc5, 0x64, 0xf2, 0x3c,
-	0xe0, 0x32, 0xcb, 0x64, 0xfe, 0x2c, 0x55, 0x05, 0x0f, 0x52, 0x19, 0x34, 0xc7, 0xcf, 0x4f, 0x78,
-	0x3b, 0xb0, 0x6f, 0xf8, 0xd5, 0x9f, 0x00, 0x00, 0x00, 0xff, 0xff, 0x6c, 0x3f, 0xd4, 0xc8, 0xda,
-	0x02, 0x00, 0x00,
+	// 401 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xa4, 0x92, 0xbd, 0x6e, 0xd5, 0x40,
+	0x10, 0x85, 0xf1, 0xfd, 0xcb, 0xcd, 0xdc, 0x1f, 0xc2, 0x04, 0x45, 0xab, 0x14, 0xc8, 0xba, 0xa2,
+	0x70, 0x83, 0xcd, 0x8f, 0x10, 0x4a, 0x85, 0x14, 0x20, 0x88, 0x0a, 0xc9, 0x88, 0x86, 0xce, 0x59,
+	0x06, 0xdf, 0x15, 0xd7, 0xde, 0xd5, 0xee, 0x26, 0xc2, 0x7e, 0x30, 0x1e, 0x88, 0x27, 0x41, 0x3b,
+	0xc6, 0x96, 0x53, 0x50, 0x20, 0x1a, 0xeb, 0x9c, 0xcf, 0x67, 0xcf, 0x14, 0x33, 0x70, 0x7a, 0xe3,
+	0xc8, 0x66, 0xe1, 0xf3, 0x4d, 0x1d, 0x28, 0x35, 0x56, 0x7b, 0x8d, 0x4b, 0x47, 0xf6, 0x56, 0x49,
+	0x72, 0xbb, 0x9f, 0x53, 0x58, 0xbf, 0xb7, 0x46, 0x7e, 0x76, 0x64, 0xaf, 0xd4, 0x81, 0xf0, 0x1c,
+	0x96, 0xea, 0x2b, 0xd5, 0x5e, 0xf9, 0x46, 0x44, 0x71, 0x94, 0x1c, 0xe7, 0x83, 0xc7, 0xc7, 0xb0,
+	0x71, 0x5e, 0x5b, 0xfa, 0xd0, 0x07, 0x26, 0x1c, 0xb8, 0x0b, 0x71, 0x07, 0xeb, 0x30, 0x6e, 0x08,
+	0x4d, 0xe3, 0x28, 0x99, 0xe5, 0x77, 0x18, 0x22, 0xcc, 0x4c, 0xe1, 0xf7, 0x62, 0xc6, 0x05, 0xac,
+	0x03, 0xab, 0x8b, 0x8a, 0xc4, 0xbc, 0x63, 0x41, 0xe3, 0x09, 0x4c, 0xe9, 0x87, 0x17, 0x0b, 0x46,
+	0x41, 0x86, 0x94, 0x53, 0x2d, 0x89, 0x23, 0x6e, 0x65, 0x8d, 0x67, 0xb0, 0x30, 0x85, 0xa5, 0xda,
+	0x8b, 0x25, 0x07, 0xff, 0xb8, 0x90, 0xf5, 0x8d, 0x21, 0x71, 0x1c, 0x47, 0xc9, 0x3c, 0x67, 0x8d,
+	0x31, 0xac, 0x94, 0x7b, 0xab, 0x2c, 0x49, 0xaf, 0x6d, 0x23, 0x20, 0x8e, 0x92, 0x65, 0x3e, 0x46,
+	0xf8, 0x10, 0xe6, 0x85, 0x57, 0x15, 0x89, 0x15, 0x8f, 0xe8, 0x4c, 0xa0, 0x92, 0xe9, 0xba, 0xa3,
+	0xb2, 0xa7, 0x15, 0xd3, 0x4d, 0x47, 0xd9, 0xa0, 0x80, 0xa3, 0x5b, 0xb2, 0x4e, 0xe9, 0x5a, 0x6c,
+	0xe3, 0x28, 0xd9, 0xe4, 0xbd, 0x0d, 0x7f, 0x0e, 0x5a, 0x7e, 0x57, 0x75, 0x29, 0xee, 0xf3, 0xe4,
+	0xde, 0xe2, 0x16, 0x26, 0xda, 0x88, 0x13, 0x8e, 0x4f, 0xb4, 0xc1, 0x47, 0x00, 0xaa, 0xac, 0xb5,
+	0xa5, 0x37, 0x85, 0x23, 0xf1, 0x80, 0xc3, 0x23, 0xf2, 0xfc, 0x57, 0x04, 0xa7, 0xe3, 0xc5, 0x7d,
+	0xea, 0x36, 0x8a, 0x97, 0xb0, 0x95, 0x96, 0x0a, 0x4f, 0xc3, 0x46, 0xcf, 0xd2, 0x7e, 0xdb, 0xe9,
+	0xf8, 0xc1, 0xf9, 0x5f, 0xf8, 0xee, 0x1e, 0xbe, 0x86, 0x55, 0x49, 0xfe, 0x3f, 0x0a, 0xae, 0x00,
+	0x47, 0x05, 0x1f, 0xed, 0xbb, 0xca, 0xf8, 0xe6, 0xdf, 0x7b, 0x2e, 0x5f, 0x7d, 0x79, 0x59, 0x2a,
+	0xbf, 0xbf, 0xb9, 0x4e, 0xa5, 0xae, 0xb2, 0xb6, 0x6d, 0xf7, 0xf6, 0xd9, 0xc5, 0xc5, 0xd3, 0x4c,
+	0xea, 0xaa, 0xd2, 0xf5, 0x93, 0xd2, 0x1a, 0x99, 0x95, 0x3a, 0xeb, 0x9f, 0x0f, 0x67, 0x7e, 0xbd,
+	0xe0, 0x3b, 0x7f, 0xf1, 0x3b, 0x00, 0x00, 0xff, 0xff, 0x34, 0x01, 0xf2, 0xb1, 0xfe, 0x02, 0x00,
+	0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -235,10 +233,10 @@ var _ grpc.ClientConn
 // is compatible with the grpc package it is being compiled against.
 const _ = grpc.SupportPackageIsVersion4
 
-// GrpcUserServiceClient is the client API for GrpcUserService service.
+// GrpcUserFileServiceClient is the client API for GrpcUserFileService service.
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
-type GrpcUserServiceClient interface {
+type GrpcUserFileServiceClient interface {
 	// rpc createUser (GrpcUser) returns (BoolEntity) {}
 	// rpc getUser (Uint64Entity) returns (GrpcUser) {}
 	CreateUserFile(ctx context.Context, in *GrpcUserFile, opts ...grpc.CallOption) (*GrpcUserFile, error)
@@ -246,43 +244,43 @@ type GrpcUserServiceClient interface {
 	GetUserFileOrEmpty(ctx context.Context, in *GrpcUserFile, opts ...grpc.CallOption) (*GrpcUserFile, error)
 }
 
-type grpcUserServiceClient struct {
+type grpcUserFileServiceClient struct {
 	cc *grpc.ClientConn
 }
 
-func NewGrpcUserServiceClient(cc *grpc.ClientConn) GrpcUserServiceClient {
-	return &grpcUserServiceClient{cc}
+func NewGrpcUserFileServiceClient(cc *grpc.ClientConn) GrpcUserFileServiceClient {
+	return &grpcUserFileServiceClient{cc}
 }
 
-func (c *grpcUserServiceClient) CreateUserFile(ctx context.Context, in *GrpcUserFile, opts ...grpc.CallOption) (*GrpcUserFile, error) {
+func (c *grpcUserFileServiceClient) CreateUserFile(ctx context.Context, in *GrpcUserFile, opts ...grpc.CallOption) (*GrpcUserFile, error) {
 	out := new(GrpcUserFile)
-	err := c.cc.Invoke(ctx, "/services.GrpcUserService/createUserFile", in, out, opts...)
+	err := c.cc.Invoke(ctx, "/services.GrpcUserFileService/createUserFile", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-func (c *grpcUserServiceClient) GetUserFile(ctx context.Context, in *GrpcUserFile, opts ...grpc.CallOption) (*GrpcUserFile, error) {
+func (c *grpcUserFileServiceClient) GetUserFile(ctx context.Context, in *GrpcUserFile, opts ...grpc.CallOption) (*GrpcUserFile, error) {
 	out := new(GrpcUserFile)
-	err := c.cc.Invoke(ctx, "/services.GrpcUserService/getUserFile", in, out, opts...)
+	err := c.cc.Invoke(ctx, "/services.GrpcUserFileService/getUserFile", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-func (c *grpcUserServiceClient) GetUserFileOrEmpty(ctx context.Context, in *GrpcUserFile, opts ...grpc.CallOption) (*GrpcUserFile, error) {
+func (c *grpcUserFileServiceClient) GetUserFileOrEmpty(ctx context.Context, in *GrpcUserFile, opts ...grpc.CallOption) (*GrpcUserFile, error) {
 	out := new(GrpcUserFile)
-	err := c.cc.Invoke(ctx, "/services.GrpcUserService/getUserFileOrEmpty", in, out, opts...)
+	err := c.cc.Invoke(ctx, "/services.GrpcUserFileService/getUserFileOrEmpty", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-// GrpcUserServiceServer is the server API for GrpcUserService service.
-type GrpcUserServiceServer interface {
+// GrpcUserFileServiceServer is the server API for GrpcUserFileService service.
+type GrpcUserFileServiceServer interface {
 	// rpc createUser (GrpcUser) returns (BoolEntity) {}
 	// rpc getUser (Uint64Entity) returns (GrpcUser) {}
 	CreateUserFile(context.Context, *GrpcUserFile) (*GrpcUserFile, error)
@@ -290,79 +288,79 @@ type GrpcUserServiceServer interface {
 	GetUserFileOrEmpty(context.Context, *GrpcUserFile) (*GrpcUserFile, error)
 }
 
-func RegisterGrpcUserServiceServer(s *grpc.Server, srv GrpcUserServiceServer) {
-	s.RegisterService(&_GrpcUserService_serviceDesc, srv)
+func RegisterGrpcUserFileServiceServer(s *grpc.Server, srv GrpcUserFileServiceServer) {
+	s.RegisterService(&_GrpcUserFileService_serviceDesc, srv)
 }
 
-func _GrpcUserService_CreateUserFile_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _GrpcUserFileService_CreateUserFile_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(GrpcUserFile)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(GrpcUserServiceServer).CreateUserFile(ctx, in)
+		return srv.(GrpcUserFileServiceServer).CreateUserFile(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/services.GrpcUserService/CreateUserFile",
+		FullMethod: "/services.GrpcUserFileService/CreateUserFile",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(GrpcUserServiceServer).CreateUserFile(ctx, req.(*GrpcUserFile))
+		return srv.(GrpcUserFileServiceServer).CreateUserFile(ctx, req.(*GrpcUserFile))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-func _GrpcUserService_GetUserFile_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _GrpcUserFileService_GetUserFile_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(GrpcUserFile)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(GrpcUserServiceServer).GetUserFile(ctx, in)
+		return srv.(GrpcUserFileServiceServer).GetUserFile(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/services.GrpcUserService/GetUserFile",
+		FullMethod: "/services.GrpcUserFileService/GetUserFile",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(GrpcUserServiceServer).GetUserFile(ctx, req.(*GrpcUserFile))
+		return srv.(GrpcUserFileServiceServer).GetUserFile(ctx, req.(*GrpcUserFile))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-func _GrpcUserService_GetUserFileOrEmpty_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _GrpcUserFileService_GetUserFileOrEmpty_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(GrpcUserFile)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(GrpcUserServiceServer).GetUserFileOrEmpty(ctx, in)
+		return srv.(GrpcUserFileServiceServer).GetUserFileOrEmpty(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/services.GrpcUserService/GetUserFileOrEmpty",
+		FullMethod: "/services.GrpcUserFileService/GetUserFileOrEmpty",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(GrpcUserServiceServer).GetUserFileOrEmpty(ctx, req.(*GrpcUserFile))
+		return srv.(GrpcUserFileServiceServer).GetUserFileOrEmpty(ctx, req.(*GrpcUserFile))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-var _GrpcUserService_serviceDesc = grpc.ServiceDesc{
-	ServiceName: "services.GrpcUserService",
-	HandlerType: (*GrpcUserServiceServer)(nil),
+var _GrpcUserFileService_serviceDesc = grpc.ServiceDesc{
+	ServiceName: "services.GrpcUserFileService",
+	HandlerType: (*GrpcUserFileServiceServer)(nil),
 	Methods: []grpc.MethodDesc{
 		{
 			MethodName: "createUserFile",
-			Handler:    _GrpcUserService_CreateUserFile_Handler,
+			Handler:    _GrpcUserFileService_CreateUserFile_Handler,
 		},
 		{
 			MethodName: "getUserFile",
-			Handler:    _GrpcUserService_GetUserFile_Handler,
+			Handler:    _GrpcUserFileService_GetUserFile_Handler,
 		},
 		{
 			MethodName: "getUserFileOrEmpty",
-			Handler:    _GrpcUserService_GetUserFileOrEmpty_Handler,
+			Handler:    _GrpcUserFileService_GetUserFileOrEmpty_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
