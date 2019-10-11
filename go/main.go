324822fd3b949166f65f103a29bacba33fb27a71
zzzhr <zzzhr@hotmail.com>
2019-10-12 01:54:28 +0800

    fix sth

--- go/main.go
+++ go/main.go
@@ -1,12 +1,7 @@
 package main
 
-// "github.com/zzzhr1990/common-grpc/go/services/user/info"
-// proto "github.com/golang/protobuf/proto"
-// "encoding/json"
-
 func main() {
-	// usr := &info.User{
-
-	// }
-	// json.Unmarshal()
+	// pbs := &User{}
+	// 	jsonpb.UnmarshalString("{\"createAddr\":\"aaab\"}", pbs)
+	//log.Printf(pbs.CreateAddr)
 }
--- main.go
+++ main.go
@@ -1,20 +1,7 @@
 package main
 
-import (
-	"encoding/json"
-	"log"
-
-	"github.com/zzzhr1990/common-grpc/go/services/user/info"
-)
-
-// "github.com/zzzhr1990/common-grpc/go/services/user/info"
-// proto "github.com/golang/protobuf/proto"
-// "encoding/json"
-
 func main() {
-	usr := &info.User{
-		CreateAddr: "aaaa",
-	}
-	b, _ := json.Marshal(usr)
-	log.Println(string(b))
+	// pbs := &User{}
+	// 	jsonpb.UnmarshalString("{\"createAddr\":\"aaab\"}", pbs)
+	//log.Printf(pbs.CreateAddr)
 }
