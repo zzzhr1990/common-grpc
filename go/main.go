b345b41ae2778ae62d7a8671c36171b1e518f431
zzzhr <zzzhr@hotmail.com>
2019-10-12 01:22:07 +0800

    add new ifo

--- go/main.go
+++ go/main.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+// "github.com/zzzhr1990/common-grpc/go/services/user/info"
+// proto "github.com/golang/protobuf/proto"
+// "encoding/json"
 
+func main() {
+	// usr := &info.User{
+
+	// }
+	// json.Unmarshal()
 }
--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"log"
 
+	"github.com/zzzhr1990/common-grpc/go/services/user/info"
+)
+
+// "github.com/zzzhr1990/common-grpc/go/services/user/info"
+// proto "github.com/golang/protobuf/proto"
+// "encoding/json"
+
+func main() {
+	usr := &info.User{
+		CreateAddr: "aaaa",
+	}
+	b, _ := json.Marshal(usr)
+	log.Println(string(b))
 }
