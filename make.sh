0616f88210944e5e7f9b204378b8b0a0450e6776
zzzhr <zzzhr@hotmail.com>
2020-06-18 21:30:40 +0800

    fix imp of grpc_js

--- make.sh
+++ make.sh
@@ -31,7 +31,7 @@ function makeFile(){
     file=$1
     echo "Generate: ${file}"
     protoc --go_out=plugins=grpc:./go_temp ${file}
-    protoc --plugin="protoc-gen-ts=${PROTOC_GEN_TS_PATH}" --plugin=protoc-gen-grpc=${PROTOC_GEN_GRPC_PATH}  --js_out="import_style=commonjs,binary:${OUT_DIR}" --ts_out="service=grpc-node:${OUT_DIR}" --grpc_out="${OUT_DIR}" ${file}
+    protoc --plugin="protoc-gen-ts=${PROTOC_GEN_TS_PATH}" --plugin=protoc-gen-grpc=${PROTOC_GEN_GRPC_PATH}  --js_out="import_style=commonjs,binary:${OUT_DIR}" --ts_out="service=grpc-node:${OUT_DIR}" --grpc_out=grpc_js:"${OUT_DIR}" ${file}
 }
 
 
