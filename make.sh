c6993fb0edd5721ec9d4ad1529a40155b914ce78
zzzhr <zzzhr@hotmail.com>
2020-07-24 03:44:55 +0800

    bump new version for grpc

--- make.sh
+++ make.sh
@@ -31,7 +31,8 @@ function makeFile(){
     file=$1
     echo "Generate: ${file}"
     protoc --go_out=plugins=grpc:./go_temp ${file}
-    protoc --plugin="protoc-gen-ts=${PROTOC_GEN_TS_PATH}" --plugin=protoc-gen-grpc=${PROTOC_GEN_GRPC_PATH}  --js_out="import_style=commonjs,binary:${OUT_DIR}" --ts_out="service=grpc-node:${OUT_DIR}" --grpc_out=grpc_js:"${OUT_DIR}" ${file}
+    protoc --plugin=protoc-gen-grpc=${PROTOC_GEN_GRPC_PATH}  --js_out="import_style=commonjs,binary:${OUT_DIR}" --grpc_out=grpc_js:"${OUT_DIR}" ${file}
+    protoc --plugin="protoc-gen-ts=${PROTOC_GEN_TS_PATH}" --ts_out="generate_package_definition:${OUT_DIR}" ${file}
 }
 
 
